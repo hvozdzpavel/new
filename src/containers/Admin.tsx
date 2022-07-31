20b80fd655d8c3fee3d187cca243da3cf10c0596
q <perfec1@rambler.ru>
2022-07-31 09:17:29 +0300

    add routs

---
+++ src/containers/Admin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Admin() {
+    return (
+        <div className="App">
+            A123123123123131,l,bl,vbl,
+        </div>
+    );
+}
+
+export default Admin;
---
+++ src/containers/Auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth() {
+    return (
+        <div className="App">
+            авторизация
+        </div>
+    );
+}
+
+export default Auth;
